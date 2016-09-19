cd2b90be9ff7a98021fa3238e4a4902b949a50f4
Quach <quachdung2502@gmail.com>
2016-09-19 18:06:20 +0800

    fix

--- practice/main_code.py
+++ practice/main_code.py
@@ -1,17 +1,20 @@
-#from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from time import time
+import pandas as pd
 import nltk
 import random
 import string
 
 stemmer = SnowballStemmer('english')
-list_stopword = [var for var in stopwords.words('english') if var not in ['not','isn']]
+list_stopword = [var for var in stopwords.words('english') if var not in ['not', 'isn']]
 
 
 def stem_tokens(tokens, stemmer):
@@ -50,17 +53,18 @@ def preprocessData():
     labels_train = positive_result[0:400] + negative_result[0:400]
     labels_test = positive_result[400:] + negative_result[400:]
     # vectorizer
-    vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 6),
+    vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 3),
                                  stop_words=list_stopword,
                                  max_df=0.8)
     features_train_transformed = vectorizer.fit_transform(features_train).toarray()
     features_test_transformed = vectorizer.transform(features_test).toarray()
-    return features_test,features_train_transformed, features_test_transformed, labels_train, labels_test, vectorizer
+    return features_train, features_test, features_train_transformed, features_test_transformed, labels_train, labels_test, vectorizer
 
 
 def main():
-    original_features_test,features_train, features_test, labels_train, labels_test, vectorizer = preprocessData()
-    clf = RadiusNeighborsClassifier(weights='distance',algorithm='brute',radius=2)
+    original_features_train, original_features_test, features_train, features_test, labels_train, labels_test, vectorizer = preprocessData()
+    # clf = RadiusNeighborsClassifier(weights='distance', algorithm='brute', radius=2)
+    clf = MultinomialNB()
     # fit/train
     t0 = time()
     clf.fit(features_train, labels_train)
@@ -70,11 +74,19 @@ def main():
     pred = clf.predict(features_test)
     print "predicting time:", round(time() - t0, 3), "s"  # accuracy
     print metrics.accuracy_score(labels_test, pred)
-    print clf.predict(vectorizer.transform(['awesome']).toarray()) #for testing specific string
-    for i in range(1,len(pred)):  #print out all false result
+    print clf.predict(vectorizer.transform(['awesome']).toarray())  # for testing specific string
+    for i in range(1, len(pred)):  # print out all false result
         if labels_test[i] != pred[i]:
             print original_features_test[i] + '\t' + pred[i]
-    print metrics.confusion_matrix(labels_test,pred)
+    print metrics.confusion_matrix(labels_test, pred)
+    features_train_tokens = vectorizer.get_feature_names()
+    positive_count = clf.feature_count_[1, :]
+    negative_count = clf.feature_count_[0, :]
+    tokens = pd.DataFrame({'token': features_train_tokens, 'positive': positive_count, 'negative': negative_count})
+    tokens['negative_ratio'] = (tokens.negative+1) / (tokens.positive+1)
+    print tokens.sort_values('negative_ratio',ascending=False)
+
+
 
 if __name__ == "__main__":
     main()
