da61cc66479deaeb335f5b62f7c180f06a0581f1
Quach <quachdung2502@gmail.com>
2016-09-30 17:37:01 +0800

    fix test

--- practice/test.py
+++ practice/test.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.corpus import sentiwordnet as swn
-sentence = "The little yellow dog barked at the cat "
+sentence = "The food in the restaurant is good but the service is bad"
 list_word = nltk.word_tokenize(sentence)
 postagger = nltk.pos_tag(list_word)
 groucho_grammar =  r"""
@@ -8,8 +8,10 @@ groucho_grammar =  r"""
         PP: {<IN|TO|RP><NP>}
         VP: {<RB|RBS|WRB>*<MD>*<TO>*<VB.*>+<RB.*|WRB>*<RP>*<NP|PP|CL>*$}
         CL: {<NP><VP>}
+            {<NP><.*>?<VB.*><JJ.*>}
         """
 print postagger
 parser = nltk.RegexpParser(groucho_grammar)
 result = parser.parse(postagger)
 print result
+result.draw()
