abdee1356cb8c65a1c0f052b1f38f3f58b37b49b
Quach <quachdung2502@gmail.com>
2016-10-04 18:06:59 +0800

    fix

--- practice/test.py
+++ practice/test.py
@@ -1,40 +1,53 @@
 import nltk
+from nltk.parse.stanford import StanfordParser
 from nltk.parse.stanford import StanfordDependencyParser
+
 path_to_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser.jar'
 path_to_models_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar'
-parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-sentence = "I  enjoy the service but the food is not good"
-test = parser.raw_parse(sentence)
-dep = test.next()
-print list(dep.triples())
+parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+sentence = "I enjoy the service but I enjoy the service"
+# parsed_Sentence = parser.raw_parse(sentence)
+# for line in parsed_Sentence:
+#        print line
+#        line.draw()
+#
+# parsed_Sentence = [parse.tree() for parse in dep_parser.raw_parse(sentence)]
+# print parsed_Sentence
+#
+# # GUI
+# for line in parsed_Sentence:
+#         print line
+#         line.draw()
 list_word = nltk.word_tokenize(sentence)
 postagger = nltk.pos_tag(list_word)
-# grammar = r"""
-#         NP: {<PDT>?<DT|WDT|WP\$|PRP\$>?<CD|JJ.*>*<PRP|WP|NN.*>+}
-#         PP: {<IN|TO|RP><NP>}
-#         VP: {<RB|RBS|WRB>*<MD>*<TO>*<VB.*>+<RB.*|WRB>*<RP>*<NP|PP|CL>*$}
-#         CL: {<NP><VP>}
-#             {<NP><.*>?<VB.*><RB>*<JJ.*>}
-#         """
 grammar = r"""
-    NP: #
-        {<NN><NN><NN>}
-        {<NNP><NNP><NNP>}
-        {<NNP><NNP><NN>}
-        {<NNS><NN><NN>}
-        {<FW><NN><NN>}
-        {<NN><NN>}
-        {<NNP><NNP>}
-        {<NNP><NN>}
-        {<FW><NN>}
-        {<NNS><NNS>}
-        {<NN><NNS>}
-        {<DT><JJ><NNS>}
-        {<IN><JJ><NNS>}
-        {<NN>}
-        {<NNP>}
-        {<NNS>}
-     """
+        NP:     {<PDT>?<DT|IN|WDT|WP\$|PRP\$>?<CD|JJ.*>*<PRP|WP|NN.*>+}
+        PP:     {<IN|TO|RP><NP>}
+        VP:     {<RB|RBS|WRB>*<MD>*<TO>*<VB.*>+<RB.*|WRB>*<RP>*<NP|PP|CL>*$}
+                {<VB.*><RB>*<JJ.*>$}
+        CL:     {<NP><VP>}
+         """
+# grammar = r"""
+#     NP:
+#         {<NN><NN><NN>}
+#         {<NNP><NNP><NNP>}
+#         {<NNP><NNP><NN>}
+#         {<NNS><NN><NN>}
+#         {<FW><NN><NN>}
+#         {<NN><NN>}
+#         {<NNP><NNP>}
+#         {<NNP><NN>}
+#         {<FW><NN>}
+#         {<NNS><NNS>}
+#         {<NN><NNS>}
+#         {<DT><JJ><NNS>}
+#         {<IN><JJ><NNS>}
+#         {<NN>}
+#         {<NNP>}
+#         {<NNS>}
+#         {<DT>?<JJ>*<NN>}
+#      """
 print postagger
 parser = nltk.RegexpParser(grammar)
 result = parser.parse(postagger)
