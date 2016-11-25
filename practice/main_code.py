ae27ee336c44a919df19c16746c1008fbfc8357a
Quach <quachdung2502@gmail.com>
2016-11-25 17:20:05 +0800

    fix

--- practice/main_code.py
+++ practice/main_code.py
@@ -1,16 +1,18 @@
 # from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import RadiusNeighborsClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import Normalizer
 from sklearn import metrics
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from time import time
+from copy import deepcopy
+from scipy.sparse import csr_matrix
+from pprint import pprint
 import pandas as pd
+import numpy as np
 import nltk
-
 import random
 import string
 
@@ -31,67 +33,129 @@ def tokenize(text):
     return stems
 
 
-def preprocessData():
-    with open('movie_rev.txt', 'r') as f:
-        text_list = f.readlines()
-    positive_list = []
-    negative_list = []
-    positive_result = []
-    negative_result = []
-    for each in text_list:
+def GetXYVocab(NumpSamples=-1):
+    Lines = [l.strip() for l in open('movie_rev.txt').xreadlines()]
+    Samples, Y = [], []
+    for each in Lines:
         each = each.translate(string.maketrans("", ""), string.punctuation)
         each = each.replace('\t', "").replace('\n', '').lower().strip()
+        Samples.append(each[:-1].strip())
         if each[-1] == '1':
-            positive_list.append(each[:-1].strip())
-            positive_result.append('1')
-        else:
-            negative_list.append(each[:-1].strip())
-            negative_result.append('0')
-    random.shuffle(positive_list)
-    random.shuffle(negative_list)
-    features_train = positive_list[0:400] + negative_list[0:400]
-    features_test = positive_list[400:] + negative_list[400:]
-    labels_train = positive_result[0:400] + negative_result[0:400]
-    labels_test = positive_result[400:] + negative_result[400:]
-    # vectorizer
-    vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 3),
-                                 stop_words=list_stopword,
-                                 max_df=0.8)
-    features_train_transformed = vectorizer.fit_transform(features_train).toarray()
-    features_test_transformed = vectorizer.transform(features_test).toarray()
-    return features_train, features_test, features_train_transformed, features_test_transformed, labels_train, labels_test, vectorizer
+            Y.append('1')
+        if each[-1] == '0':
+            Y.append('0')
+        if each[-1] == '2':
+            Y.append('2')
+
+    CountVecter = CountVectorizer(lowercase=False, dtype=np.float64, binary=False)  # ,max_df=0.95)
+    X = CountVecter.fit_transform(Samples)
+    X = Normalizer().fit_transform(X)
+    print 'shape of X matrixs', X.shape
+    Vocab = CountVecter.get_feature_names() + ['HLPos', 'HLNeg', 'HLSum', 'NrcPos', 'NrcNeg', 'NrcSum', 'SubjPos',
+                                               'SubjNeg', 'SubjSum']
+    return X, Y, Vocab
+
+
+    # def preprocessData():
+#     with open('movie_rev.txt', 'r') as f:
+#         text_list = f.readlines()
+#     positive_list = []
+#     negative_list = []
+#     positive_result = []
+#     negative_result = []
+#     for each in text_list:
+#         each = each.translate(string.maketrans("", ""), string.punctuation)
+#         each = each.replace('\t', "").replace('\n', '').lower().strip()
+#         if each[-1] == '1':
+#             positive_list.append(each[:-1].strip())
+#             positive_result.append('1')
+#         else:
+#             negative_list.append(each[:-1].strip())
+#             negative_result.append('0')
+#     random.shuffle(positive_list)
+#     random.shuffle(negative_list)
+#     features_train = positive_list[0:400] + negative_list[0:400]
+#     features_test = positive_list[400:] + negative_list[400:]
+#     labels_train = positive_result[0:400] + negative_result[0:400]
+#     labels_test = positive_result[400:] + negative_result[400:]
+#     # vectorizer
+#     vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 3),
+#                                  stop_words=list_stopword,
+#                                  max_df=0.8)
+#     features_train_transformed = vectorizer.fit_transform(features_train).toarray()
+#     features_test_transformed = vectorizer.transform(features_test).toarray()
+#     # CountVecter = CountVectorizer(lowercase=False, dtype=np.float64, binary=False)  # ,max_df=0.95)
+#     # X = CountVecter.fit_transform(features_train)
+#     # X = Normalizer().fit_transform(X)
+#     # Vocab = CountVecter.get_feature_names()
+#     # Y = labels_train
+#     return features_train, features_test, features_train_transformed, features_test_transformed, labels_train, labels_test, vectorizer, Vocab
+
+
+def GetTopN(W, Vocab, N=20):
+    FeatsAndVocab = zip(W.tolist(), Vocab)
+    FeatsAndVocab.sort()
+    FeatsAndVocab.reverse()
+    return FeatsAndVocab[:N]
+
+
+def AnalyseClassifierFeats(Classifier, Vocab, TopN=20):
+    W = deepcopy(Classifier.coef_)
+    NegW = W[0, :]
+    NeuW = W[1, :]
+    PosW = W[2, :]
+
+    TopNeg = GetTopN(NegW, Vocab, TopN)
+    TopNeu = GetTopN(NeuW, Vocab, TopN)
+    TopPos = GetTopN(PosW, Vocab, TopN)
+    # TopConf =  GetTopN(ConfW, Vocab, TopN)
+    # return TopNeg, TopNeu, TopPos, TopConf
+    return TopNeg, TopNeu,TopPos
 
 
 def main():
-    original_features_train, original_features_test, features_train, features_test, labels_train, labels_test, vectorizer = preprocessData()
+    # original_features_train, original_features_test, features_train, features_test, labels_train, labels_test, vectorizer, Vocab = preprocessData()
+    X, Y, Vocab = GetXYVocab(-1)
     # clf = RadiusNeighborsClassifier(weights='distance', algorithm='brute', radius=2)
-    clf = MultinomialNB()
+    clf = LinearSVC(C=0.01)
     # fit/train
-    t0 = time()
-    clf.fit(features_train, labels_train)
-    print "training time:", round(time() - t0, 3), "s"
-    # predict
-    t0 = time()
-    pred = clf.predict(features_test)
-    print "predicting time:", round(time() - t0, 3), "s"  # accuracy
-    print metrics.accuracy_score(labels_test, pred)
+    # t0 = time()
+    # clf.fit(features_train, labels_train)
+    # print "training time:", round(time() - t0, 3), "s"
+    # # predict
+    # t0 = time()
+    # pred = clf.predict(features_test)
+    # print "predicting time:", round(time() - t0, 3), "s"  # accuracy
+    # print metrics.accuracy_score(labels_test, pred)
+    clf.fit(X, Y)
 
-    print clf.predict(vectorizer.transform(['awesome']).toarray())  # for testing specific string
+    X = csr_matrix(X)
+    TopN = 20
+    # TopNeg, TopNeu, TopPos, TopConf = AnalyseClassifierFeats (Classifier, Vocab, TopN)
+    TopNeg, TopNeu, TopPos = AnalyseClassifierFeats(clf, Vocab, TopN)
+    print '*' * 80
+    print 'top {} pos feats: '.format(TopN);
+    pprint(TopPos);
+    print '*' * 80
+    print 'top {} neg feats: '.format(TopN);
+    pprint(TopNeg);
+    print '*' * 80
+    print 'top {} neu feats: '.format(TopN);
+    pprint(TopNeu);
+    print '*' * 80
+
+    # print clf.predict(vectorizer.transform(['awesome', 'not bad']).toarray())  # for testing specific string
 
     # for i in range(1, len(pred)):  # print out all false result
     #     if labels_test[i] != pred[i]:
     #         print original_features_test[i] + '\t' + pred[i]
-    print original_features_train[0]
-    for each in features_train[0]:
-        print each
-    #print metrics.confusion_matrix(labels_test, pred)
+    # print metrics.confusion_matrix(labels_test, pred)
     # features_train_tokens = vectorizer.get_feature_names()
     # positive_count = clf.feature_count_[1, :]
     # negative_count = clf.feature_count_[0, :]
     # tokens = pd.DataFrame({'token': features_train_tokens, 'positive': positive_count, 'negative': negative_count})
     # tokens['negative_ratio'] = (tokens.negative+1) / (tokens.positive+1)
     # print tokens.sort_values('negative_ratio',ascending=False)
-
 
 
 if __name__ == "__main__":
--- practice/test.py
+++ practice/test.py
@@ -5,51 +5,158 @@ import string
 import networkx as nx
 from nltk.stem.wordnet import WordNetLemmatizer
 import matplotlib.pyplot as plt
+from xml.etree import ElementTree as ET
+
 # from nltk.stem.snowball import SnowballStemmer
+dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+
+def ParseContext(sentence, aspect_term, from_char, to_char):
+    from_index, to_index = PositionOfAspectTerm(sentence, aspect_term, from_char, to_char)
+    source_fromIndex = str(from_index + 1)
+    source_toIndex = str(to_index + 1)
+    parse_sent = dep_parser.raw_parse(sentence)
+    dep = parse_sent.next()
+    G = nx.Graph()
+    context = []
+    for each in dep.to_dot().split("\n")[4:-1]:  # Use index [4:-1] to get the text only
+        # Convert the input from unicode to text
+        each = str(each)
+        # Remove all the punctuations except ">","=" to identify the relationship in the graph
+        each = each.translate(string.maketrans("", ""), string.punctuation.replace(">", "").replace("=", ""))
+        # If there is ">" the text is about the relationship( edge)
+        if ">" in each:
+            relationship = each.split("=")[-1]
+            each = each.split()
+            G.add_edge(each[0], each[2], label=relationship)
+        else:
+            each = each.split()
+            G.add_node(each[0], name=each[-1])
+    name_attribute = nx.get_node_attributes(G, 'name')
+    parse_context_fromIndex = nx.single_source_shortest_path_length(G, source_fromIndex, cutoff=3)
+    for each in parse_context_fromIndex.keys():
+        condition = int(each)
+        if (condition > 0 and (condition <= (from_index+1) or condition >= (to_index+1))):
+            context.append(name_attribute[each])
+    if (to_index != from_index):
+        parse_context_toIndex = nx.single_source_shortest_path_length(G, source_toIndex, cutoff=3)
+        for each in parse_context_toIndex.keys():
+            condition = int(each)
+            if (condition > 0 and (condition < (from_index + 1) or condition > (to_index + 1))):
+                context.append(name_attribute[each])
+    return list(set(context))  # Return the nodes (words) in the parse tree that are connected to the aspect term by at most three edges
+def ParseFeatures(sentence, aspect_term, from_char, to_char):
+    parseContext = ParseContext(sentence, aspect_term, from_char, to_char)
+    # dict_postag = {'word1':'POSTAG1','word2':'POSTAG2'}
+    result = []
+    dict_postag = {}
+    postag = nltk.pos_tag(nltk.word_tokenize(sentence))
+    # Create a dictionary with words as keys and POS_TAG as values
+    for each in postag:
+        dict_postag[each[0]] = each[1]
+    for each in parseContext:
+        result.append(each + '_' + dict_postag[each])  # Word_POSTAG in the parse context
+    slice = sentence.partition(aspect_term)
+    for word in parseContext:
+        # Determine whether the word is before or after the aspect term to form the correct bigrams context target
+        if word in slice[0]:
+            result.append(word + "_" + aspect_term + "_ct")
+        elif word in slice[2]:
+            result.append(aspect_term + "_" + word + "_ct")
+    return result
+def PositionOfAspectTerm(sentence, aspect_term, from_char,
+                         to_char):  # Return the position of aspect term in a given sentence with given from/to positions of character
+    list_sentence = sentence.split()
+    from_index = 0
+    to_index = 0
+    position = 0
+    # from_index and to_index is the position of the aspect term in the list
+    for each in list_sentence:
+        if (position + len(each) + 1) <= int(from_char):
+            position += len(each) + 1
+        else:
+            from_index = list_sentence.index(each)
+            break
+    position = 0
+    # if the aspect term is only 1 word so from_index = to_index
+    if len(aspect_term.split()) == 1:
+        to_index = from_index
+    else:
+        for each in list_sentence:
+            if position + len(each) + 1 < int(to_char):
+                position += len(each) + 1
+            else:
+                to_index = list_sentence.index(each)
+                break
+    return from_index, to_index
+
 
 # path_to_jar = 'D:\stanford-parser-full-2015-12-09\stanford-parser-full-2015-12-09\stanford-parser.jar'
 # path_to_models_jar = 'D:\stanford-parser-full-2015-12-09\stanford-parser-full-2015-12-09\stanford-parser-3.6.0-models.jar'
-parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-sentence = "The decor is vibrant and eye-pleasing with several semi-private boths on the right side of the dining hall, which are great for a date"
-aspect_term = "decor"
-from_index = 1
-to_index = 1
-list_word = nltk.word_tokenize(sentence)
-postag = nltk.pos_tag(list_word)
-dict_postag = {}
-for each in postag:
-    dict_postag[each[0]] = each[1]
-result = dep_parser.raw_parse(sentence)
-dep = result.next()
-G = nx.Graph()
-# a = list(dep.triples())
-# for triple in dep.triples():
-#     print triple[1],"(",triple[0][0],", ",triple[2][0],")"
+def SentenceTransform(sentence, aspect_term, from_char, to_char, window_size):
+    parse_feats = ParseFeatures(sentence, aspect_term, from_char, to_char)
+    return ' '.join(parse_feats)
+
+
+def PreprocessData():
+    text = []
+    tree = ET.parse('restaurants-trial.xml')
+    root = tree.getroot()
+    for sentence in root.findall('sentence'):
+        if sentence.find('aspectTerms') is None:
+            continue
+        content = sentence.find('text').text.translate(string.maketrans("", ""), string.punctuation)
+        for aspectTerms in sentence.iter('aspectTerms'):
+            for aspectTerm in aspectTerms.iter('aspectTerm'):
+                text.append(SentenceTransform(content, aspectTerm.get('term').translate(string.maketrans("", ""),
+                                                                                        string.punctuation),
+                                              aspectTerm.get("from"), aspectTerm.get("to"), 10))
+    return text
+def main():
+    print len(PreprocessData())
+if __name__ == "__main__":
+    main()
+
+# sentence = "The decor is vibrant and eye-pleasing with several semi-private boths on the right side of the dining hall, which are great for a date"
+# aspect_term = "decor"
+# from_index = 1
+# to_index = 1
+# list_word = nltk.word_tokenize(sentence)
+# postag = nltk.pos_tag(list_word)
+# dict_postag = {}
+# for each in postag:
+#     dict_postag[each[0]] = each[1]
+# result = dep_parser.raw_parse(sentence)
+# dep = result.next()
+# G = nx.Graph()
+# # a = list(dep.triples())
+# # for triple in dep.triples():
+# #     print triple[1],"(",triple[0][0],", ",triple[2][0],")"
+# # for each in dep.to_dot().split("\n")[4:-1]:
+# #     each = str(each)
+# #     each = each.translate(string.maketrans("", ""), string.punctuation.replace(">", "").replace("=", ""))
+# #     if ">" not in each:
+# #         each = each.split()
+# #         label_node[each[0]] = each[-1]
 # for each in dep.to_dot().split("\n")[4:-1]:
 #     each = str(each)
 #     each = each.translate(string.maketrans("", ""), string.punctuation.replace(">", "").replace("=", ""))
-#     if ">" not in each:
+#     if ">" in each:
+#         relationship = each.split("=")[-1]
 #         each = each.split()
-#         label_node[each[0]] = each[-1]
-for each in dep.to_dot().split("\n")[4:-1]:
-    each = str(each)
-    each = each.translate(string.maketrans("", ""), string.punctuation.replace(">", "").replace("=", ""))
-    if ">" in each:
-        relationship = each.split("=")[-1]
-        each = each.split()
-        G.add_edge(each[0], each[2], label = relationship)
-    else:
-        each = each.split()
-        G.add_node(each[0], name = each[-1])
-name_attribute = nx.get_node_attributes(G,'name')
-print G.nodes()
-source_fromIndex = str(from_index +1)
-target_toIndex = str(to_index+1)
-parse_context_length = nx.single_source_shortest_path_length(G,source,cutoff = 3)
-print length
-for each in length.keys():
-    print name_attribute[each]
+#         G.add_edge(each[0], each[2], label = relationship)
+#     else:
+#         each = each.split()
+#         G.add_node(each[0], name = each[-1])
+# name_attribute = nx.get_node_attributes(G,'name')
+# source_fromIndex = str(from_index +1)
+# target_toIndex = str(to_index+1)
+# parse_context_length = nx.single_source_shortest_path_length(G,source_fromIndex,cutoff = 3)
+# for each in parse_context_length.keys():
+#     if each != source_fromIndex:
+#         for path in nx.all_simple_paths(G, source=source_fromIndex, target=each):
+#             for element in path:
+#                 print name_attribute[element]+'_',
+#             print
 # for each in G.nodes():
 #      print
 # nx.draw_networkx_edge_labels(DG,pos,edge_labels=edge_labels)
